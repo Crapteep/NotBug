917ae0905c46aab86001f2abbb20cc341fed210d
Crapteep <crapteep@gmail.com>
2024-11-01 00:57:43 +0100

    task models have been created

---
+++ todo-flask/app/models.py
@@ -0,0 +1,18 @@
+
+import uuid
+from sqlmodel import Field, SQLModel
+
+
+
+class TaskBase(SQLModel):
+    title: str = Field(max_length=80)
+    description: str | None = Field(default=None, max_length=200)
+    completed: bool = Field(default=False)
+
+
+class Task(TaskBase, table=True):
+    id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
+
+
+class TaskCreate(TaskBase):
+    pass
