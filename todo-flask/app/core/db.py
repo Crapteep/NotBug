dcf5d4b667789254dd6393780557c9261baae72a
Crapteep <crapteep@gmail.com>
2024-11-01 14:36:54 +0100

    minor update in core

--- todo-flask/app/core/db.py
+++ todo-flask/app/core/db.py
@@ -1,21 +1,9 @@
-from sqlmodel import Session, create_engine, select
-
-from app import crud
+from sqlmodel import create_engine
 from app.core.config import settings
-from app.models import User, UserCreate
+
 
 engine = create_engine(str(settings.SQLALCHEMY_DATABASE_URI))
 
+from sqlmodel import SQLModel
 
-
-def init_db(session: Session) -> None:
-    user = session.exec(
-        select(User).where(User.email == settings.FIRST_SUPERUSER)
-    ).first()
-    if not user:
-        user_in = UserCreate(
-            email=settings.FIRST_SUPERUSER,
-            password=settings.FIRST_SUPERUSER_PASSWORD,
-            is_superuser=True,
-        )
-        user = crud.create_user(session=session, user_create=user_in)
+SQLModel.metadata.create_all(engine)
